60e9cb74149950ac6093c188b125fbcccde8a7e3
Tofulati <almtho2003@gmail.com>
2022-10-04 19:29:39 -0700

    Updated for Week 5 lab

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -9,14 +9,31 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var flashcardDatabase: FlashcardDatabase
+    var allFlashcards = mutableListOf<Flashcard>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        flashcardDatabase = FlashcardDatabase(this)
+        allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flascard_answer)
         val addAddButton = findViewById<ImageView>(R.id.add_question_button)
+        val nextQuestion = findViewById<ImageView>(R.id.next_question)
+        var currentCardDisplayedIndex = 0
+
+        if (allFlashcards.size > 0) {
+            flashcardQuestion.text = allFlashcards[0].question
+            flashcardAnswer.text = allFlashcards[0].answer
+        }
+
+        flashcardDatabase = FlashcardDatabase(this)
 
         flashcardAnswer.visibility = View.INVISIBLE
         flashcardQuestion.setOnClickListener {
@@ -29,10 +46,36 @@ class MainActivity : AppCompatActivity() {
             flashcardAnswer.visibility = View.INVISIBLE
         }
 
+        nextQuestion.setOnClickListener {
+            if (allFlashcards.size == 0) {
+                // return here, so that the rest of the code in this onClickListener doesn't execute
+                return@setOnClickListener
+            }
+            // advance our pointer index so we can show the next card
+            currentCardDisplayedIndex++
+            // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+            if(currentCardDisplayedIndex >= allFlashcards.size) {
+                Snackbar.make(
+                    findViewById<TextView>(R.id.flashcard_question), // This should be the TextView for displaying your flashcard question
+                    "You've reached the end of the cards, going back to start.",
+                    Snackbar.LENGTH_SHORT)
+                    .show()
+                currentCardDisplayedIndex = 0
+            }
+            // set the question and answer TextViews with data from the database
+            allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+            val (question, answer) = allFlashcards[currentCardDisplayedIndex]
+
+            flashcardAnswer.text = answer
+            flashcardQuestion.text = question
+        }
+
         val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             val data: Intent? = result.data
+
             if (data != null) { // Check that we have data returned
-                val question = data.getStringExtra("card_question") // 'string1' needs to match the key we used when we put the string in the Intent
+                val question =
+                    data.getStringExtra("card_question") // 'string1' needs to match the key we used when we put the string in the Intent
                 val answer = data.getStringExtra("card_answer")
 
                 flashcardQuestion.text = question
@@ -40,6 +83,17 @@ class MainActivity : AppCompatActivity() {
                 // Log the value of the strings for easier debugging
                 Log.i("MainActivity", "question: $question")
                 Log.i("MainActivity", "answer: $answer")
+
+                if (question != null && answer != null) {
+                    flashcardDatabase.insertCard(Flashcard(question, answer))
+                    // Update set of flashcards to include new card
+                    allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+                } else {
+                    Log.e(
+                        "TAG",
+                        "Missing question or answer to input into database. Question is $question and answer is $answer"
+                    )
+                }
             } else {
                 Log.i("MainActivity", "Returned null data from AddCardActivity")
             }
